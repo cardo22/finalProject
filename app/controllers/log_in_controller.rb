8d6da9335944f77123108e4d808ce0de65477ec2
Ricardo Clarke <wolfpack3371@gmail.com>
2016-05-05 15:39:15 -0400

    change home route

---
+++ app/controllers/log_in_controller.rb
@@ -0,0 +1,5 @@
+class LogInController < ApplicationController
+  def home
+  	render 'home'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'log_in#home'
+  get 'log_in/home' => 'log_in#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
