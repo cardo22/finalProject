d338d9108a6a12126ed45939ca918f414eaf9a37
Ricardo Clarke <wolfpack3371@gmail.com>
2016-05-10 14:07:57 -0400

    working on groups controller create method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,4 +5,25 @@ class GroupsController < ApplicationController
 		@groups = user.groups
 		render 'group_page'
 	end
+
+	def new
+		@groups = Group.new
+		render "new"
+	end
+
+	def create
+		user = current_user
+		@groups = Group.new(group_params)
+		if @groups.save
+			redirect_to action: "show", controller: "groups"
+		else
+			render "new"
+		end
+	end
+
+	private
+
+	def group_params
+		params.require(:groups).permit(:name, :card)
+	end
 end
