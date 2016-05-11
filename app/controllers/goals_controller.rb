f8b30e2eb6f0fa06f7dbd62643fce5d12fbfc5dd
Ricardo Clarke <wolfpack3371@gmail.com>
2016-05-11 12:29:51 -0400

    doing crud for goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -9,8 +9,31 @@ class GoalsController < ApplicationController
 		render "new"
 	end
 
+	def create
+		unless current_user.admin
+			redirect_to action: "show", controller: "groups"
+			return
+		end
+
+		user = current_user
+		new_goal = Goal.new(goals_params)
+		@goals = user.goals.push(new_goal)
+		if new_goal.save
+			redirect_to "/goals/#{new_goal.id}/edit"
+		else
+			render "new"
+		end
+	end
+
 	def edit
-		
+		unless current_user.admin
+			redirect_to action: "show", controller: "goals"
+			return
+		end
+
+		user = current_user
+		@goals = user.goals.find(params[:id])
+		render 'edit'
 	end
 
 	private
