7881f8470c16a926f00e5d89ace27f3502579c18
Ricardo Clarke <wolfpack3371@gmail.com>
2016-05-18 09:59:52 -0400

    fronted stuff

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,6 +1,6 @@
 class InvitesController < ApplicationController
 	def index
-		@group = Group.find(params[:group_id])
+		@group = Group.find_by(id: params[:group_id])
 		@invites = @group.invites.order(created_at: :asc)
 		                              .limit(20)
 		render 'invites'
